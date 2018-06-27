fba2013ede608ae26716f0b1492b382d5228ed35
TheNewStyles <tyler.peterson@utah.edu>
2018-06-27 16:18:52 -0500

    Add basic outline and TODO comments

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,38 @@ import '../styles/App.css';
 class App extends Component {
   render() {
     return (
-      <div >
-        <header></header>
-        <main>
-          <section>
-            <article></article>
+      <div >          
+        <nav id="navbar">
+          <header></header>
+          {/* make nav link component */}
+          <a className="nav-link" href=""></a>
+          <a className="nav-link" href=""></a>
+          <a className="nav-link" href=""></a>
+        </nav>       
+        {/* make main-section component */}
+          {/* Title just title and size */}
+        {/* make code component */}
+        {/* make list component */}
+        {/* add lists and code as needed */}
+        <main id="main-doc">
+          <section id="" className="main-section">
+            <header id=""></header>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </section>
+          <section id="" className="main-section">
+            <header id=""></header>
+            <p></p>
+            <p></p>
+            <p></p>
+          </section>
+          <section id="" className="main-section">
+            <header id=""></header>
+            <p></p>
+            <p></p>
+            <p></p>
           </section>
         </main>        
       </div>
