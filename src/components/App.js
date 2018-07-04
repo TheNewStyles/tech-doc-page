f7f2409843ff639d54ba06047587bdd5e576d225
TheNewStyles <tyler.peterson@utah.edu>
2018-07-04 10:35:28 -0500

    Add more sections from react docs

--- src/components/App.js
+++ src/components/App.js
@@ -17,10 +17,10 @@ class App extends Component {
       <div id="main">
         <div className="nav-container">
             <NavBar title="MAIN CONCEPTS" />
-            <NavBar title="ADVANCED GUIDES" />
+            {/* <NavBar title="ADVANCED GUIDES" />
             <NavBar title="API REFERENCE" />
             <NavBar title="CONTRIBUTING" />
-            <NavBar title="FAQ" />
+            <NavBar title="FAQ" /> */}
         </div>
         <main id="main-doc">
             <section id="hello-world" className="main-section">
@@ -172,10 +172,55 @@ class App extends Component {
                 <Paragraph text={[<strong>All React components must act like pure functions with respect to their props.</strong>]} />
                 <Paragraph text="Of course, application UIs are dynamic and change over time. In the next section, we will introduce a new concept of “state”. State allows React components to change their output over time in response to user actions, network responses, and anything else, without violating this rule." />
            </section>
+           <section id="state-and-lifecycle" className="main-section">
+                <h1>State and Lifecycle</h1>
+                <Paragraph text={["This page introduces the concept of state and lifecycle in a React component. You can find a " , <ExternalLink secondClassName="no-margin" linkText="detailed component API reference here." href="https://reactjs.org/docs/react-component.html" /> ]} />
+                <Paragraph text="Consider the ticking clock example from one of the previous sections. In Rendering Elements, we have only learned one way to update the UI. We call ReactDOM.render() to change the rendered output:" />
+                <Code code={this.code[28]} />
+                <ExternalLink linkText="Try it on CodePen" href="codepen.io"/>
+                <Paragraph text="In this section, we will learn how to make the Clock component truly reusable and encapsulated. It will set up its own timer and update itself every second." />
+                <Code code={this.code[29]} codeCaption="We can start by encapsulating how the clock looks:" />
+                <ExternalLink linkText="Try it on CodePen" href="codepen.io"/>
+                <Paragraph text="However, it misses a crucial requirement: the fact that the Clock sets up a timer and updates the UI every second should be an implementation detail of the Clock." />
+                <Code code={this.code[30]} codeCaption="Ideally we want to write this once and have the Clock update itself:" />
+                <Paragraph text={[ "To implement this, we need to add state to the Clock component. " , <br></br> , <br></br> , "State is similar to props, but it is private and fully controlled by the component. " ,  <br></br> , <br></br> , " We mentioned before that components defined as classes have some additional features. Local state is exactly that: a feature available only to classes."]} />
+                <SectionHeader title="Converting a Function to a Class" />
+                <List title="You can convert a functional component like Clock to a class in five steps:" listItems={["Create an ES6 class, with the same name, that extends React.Component." , "Add a single empty method to it called render()." , "Move the body of the function into the render() method.", "Replace props with this.props in the render() body." , "Delete the remaining empty function declaration."]} />
+                <Code code={this.code[31]} captionClass="no-caption" />
+                <ExternalLink linkText="Try it on CodePen" href="codepen.io"/>
+                <Paragraph text="Clock is now defined as a class rather than a function. The render method will be called each time an update happens, but as long as we render <Clock /> into the same DOM node, only a single instance of the Clock class will be used. This lets us use additional features such as local state and lifecycle hooks." />
+                <SectionHeader title="Adding Local State to a Class" />
+                <Paragraph text="We will move the date from props to state in three steps:" />
+                <Code code={this.code[32]} codeCaption="1. Replace this.props.date with this.state.date in the render() method:" />
+                <Code code={this.code[33]} codeCaption="2 Add a class constructor that assigns the initial this.state:" />
+                <Code code={this.code[34]} codeCaption="Note how we pass props to the base constructor:" />
+                <Paragraph text="Class components should always call the base constructor with props." />
+                <Code code={this.code[35]} codeCaption="3. Remove the date prop from the <Clock /> element:" />
+                <Paragraph text="We will later add the timer code back to the component itself." />
+                <Code code={this.code[36]} codeCaption="The result looks like this:" />
+                <ExternalLink linkText="Try it on CodePen" href="https://codepen.io/gaearon/pen/KgQpJd?editors=0010"/>
+                <Paragraph text="Next, we’ll make the Clock set up its own timer and update itself every second." />
+                <SectionHeader title="Adding Lifecycle Methods to a Class" />
+                <Paragraph text="In applications with many components, it’s very important to free up resources taken by the components when they are destroyed." />
+                <Paragraph text={["We want to " , <ExternalLink secondClassName="no-margin" linkText="setup a timer " href="https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval" /> , " whenever the Clock is rendered to the DOM for the first time. This is called “mounting” in React."]} />
+                <Paragraph text={["We also want to " ,  <ExternalLink secondClassName="no-margin" linkText="clear that timer" href="https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval" /> , " whenever the DOM produced by the Clock is removed. This is called “unmounting” in React."]} />
+                <Code code={this.code[37]} codeCaption="We can declare special methods on the component class to run some code when a component mounts and unmounts:" />
+                <Paragraph text="These methods are called “lifecycle hooks”." />
+                <Code code={this.code[38]} codeCaption="The componentDidMount() hook runs after the component output has been rendered to the DOM. This is a good place to set up a timer:" />
+                <Paragraph text="Note how we save the timer ID right on this." />
+                <Paragraph text="While this.props is set up by React itself and this.state has a special meaning, you are free to add additional fields to the class manually if you need to store something that doesn’t participate in the data flow (like a timer ID)." />
+                <Code code={this.code[39]} codeCaption="We will tear down the timer in the componentWillUnmount() lifecycle hook:" />
+                <Paragraph text="Finally, we will implement a method called tick() that the Clock component will run every second." />
+                <Code code={this.code[40]} codeCaption="It will use this.setState() to schedule updates to the component local state:" />
+                <ExternalLink secondClassName="no-margin" linkText="Try it on Codepen" href="https://codepen.io/gaearon/pen/amqdNA?editors=0010" />
+                <Paragraph text="Now the clock ticks every second." />
+                <List title="Let’s quickly recap what’s going on and the order in which the methods are called:" listItems={["1. When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock component. Since Clock needs to display the current time, it initializes this.state with an object including the current time. We will later update this state.","2. React then calls the Clock component’s render() method. This is how React learns what should be displayed on the screen. React then updates the DOM to match the Clock’s render output." , "3. When the Clock output is inserted in the DOM, React calls the componentDidMount() lifecycle hook. Inside it, the Clock component asks the browser to set up a timer to call the component’s tick() method once a second." , "4. Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update by calling setState() with an object containing the current time. Thanks to the setState() call, React knows the state has changed, and calls the render() method again to learn what should be on the screen. This time, this.state.date in the render() method will be different, and so the render output will include the updated time. React updates the DOM accordingly.", "5 If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle hook so the timer is stopped."]} />
+           </section>
         </main>
       </div>
     );
   }
+
 
 code = [
 `ReactDOM.render(
@@ -371,7 +416,174 @@ ReactDOM.render(
   }`,
   `function withdraw(account, amount) {
     account.total -= amount;
-  }`
+  }`,
+  `function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    );
+  }
+
+  setInterval(tick, 1000);`,
+  `function Clock(props) {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+
+  function tick() {
+    ReactDOM.render(
+      <Clock date={new Date()} />,
+      document.getElementById('root')
+    );
+  }
+
+  setInterval(tick, 1000);`,
+  `ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );`,
+  `class Clock extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }`,
+  `class Clock extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }`,
+  `class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }`,
+`constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }`,
+  `ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );`,
+  `class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );`,
+  `class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }`,
+  `  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }`,
+  `  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }`,
+  `class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );`
 ];
 
 
--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -5,7 +5,7 @@ import '../styles/ExternalLink.css';
 class ExternalLink extends Component {
     render() {
       return (
-        <a className="external-link" href={this.props.link}>{this.props.linkText}</a>
+        <a className={["external-link " + this.props.secondClassName]} href={this.props.link}>{this.props.linkText}</a>
         );
     }
 }
